e97593cce6d2db2bc498d17137fc9a704fe7128c
Arvic S. Babol <kulotsystems@gmail.com>
2022-05-02 11:14:00 +0800

    Tested cloneArray.

---
+++ cloneArray.test.js
@@ -0,0 +1,12 @@
+const cloneArray = require('./cloneArray.js');
+
+test('properly clones array', () => {
+    const array = [1, 2, 3];
+    expect(
+        cloneArray(array)
+    ).toEqual(array);
+
+    expect(
+        cloneArray(array)
+    ).not.toBe(array);
+});
